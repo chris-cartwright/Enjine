50842999449995b09c1a4f6c0a00bd10990c9d7c
Chris Cartwright <cartwright.christopher@gmail.com>
2019-02-06 20:06:00 -0500

    Add grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'enjine';
+  public title = 'enjine';
+
+  public columnDefs = [
+    { headerName: 'Field', field: 'field', sortable: true, filter: true },
+    { headerName: 'Left', field: 'left', sortable: true, filter: true },
+    { headerName: 'Right', field: 'right', sortable: true, filter: true },
+    { headerName: 'Difference', field: 'difference', sortable: true, filter: true }
+  ];
+
+  public rowData = [
+    { field: 'Toyota', left: '15%', right: '20%', difference: '5%' }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
